778f4f739c08c4634a698c6143627eaac750ce6a
johnoro <officialjohnorourke@gmail.com>
2019-02-15 16:48:45 -0500

    Bootstrap project and add Jackson dependencies

---
+++ src/main/java/com/lambdaschool/cities/CitiesApplication.java
@@ -0,0 +1,12 @@
+package com.lambdaschool.cities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(CitiesApplication.class, args);
+  }
+}
+
