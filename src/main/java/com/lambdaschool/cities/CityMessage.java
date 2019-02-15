d3e34e8d42297ab15ee1fa7f7b426af13451f526
johnoro <officialjohnorourke@gmail.com>
2019-02-15 18:28:08 -0500

    Add city message class

---
+++ src/main/java/com/lambdaschool/cities/CityMessage.java
@@ -0,0 +1,21 @@
+package com.lambdaschool.cities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class CityMessage implements Serializable {
+  private final String text;
+  private final int priority;
+  private final boolean secret;
+
+  public CityMessage(
+    @JsonProperty("Text") String text,
+    @JsonProperty("priority") int priority,
+    @JsonProperty("secret") boolean secret
+  ) {
+    this.text = text;
+    this.priority = priority;
+    this.secret = secret;
+  }
+}
